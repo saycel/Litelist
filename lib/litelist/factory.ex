6230ec0d77618429f28b7207dd03ac3341d6590a
Andy Gimma <andy@hylo.com>
2018-02-05 23:01:43 -0500

    Use factories to create neighbor

--- lib/litelist/factory.ex
+++ lib/litelist/factory.ex
@@ -1,12 +1,11 @@
 defmodule Litelist.Factory do
-    # with Ecto
-    use ExMachina.Ecto, repo: Litelist.Repo
+  use ExMachina.Ecto, repo: Litelist.Repo
   
-    alias Litelist.Auth.Neighbor
-    def neighbor_factory do
-      %Neighbor{
-        username: sequence(:email, &"email-#{&1}@example.com"),
-        password: "password"
-      }
-    end
+  alias Litelist.Auth.Neighbor
+  def neighbor_factory do
+    %Neighbor{
+      username: sequence(:email, &"email-#{&1}@example.com"),
+      password: "password"
+    }
   end
+end
