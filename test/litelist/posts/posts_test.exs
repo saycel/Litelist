2beb44cdb85fb3e495aa3d9e8f84af2e25e97f70
Andy Gimma <andy@hylo.com>
2018-03-11 12:42:48 -0400

    Test unique url in posts, refactor post tests to use Factories

--- test/litelist/posts/posts_test.exs
+++ test/litelist/posts/posts_test.exs
@@ -2,6 +2,7 @@ defmodule Litelist.PostsTest do
   use Litelist.DataCase
 
   alias Litelist.Posts
+  alias Litelist.Factory
   
   describe "posts" do
     alias Litelist.Posts.Post
@@ -20,12 +21,12 @@ defmodule Litelist.PostsTest do
     end
 
     test "list_posts/0 returns all posts" do
-      post = post_fixture()
+      post = Factory.insert(:for_sale, @valid_attrs)
       assert Posts.list_posts() == [post]
     end
 
     test "get_post!/1 returns the post with given id" do
-      post = post_fixture()
+      post = Factory.insert(:for_sale, @valid_attrs)
       assert Posts.get_post!(post.id) == post
     end
 
@@ -50,7 +51,7 @@ defmodule Litelist.PostsTest do
     end
 
     test "update_post/2 with valid data updates the post" do
-      post = post_fixture()
+      post = Factory.insert(:for_sale, @valid_attrs)
       assert {:ok, post} = Posts.update_post(post, @update_attrs)
       assert %Post{} = post
       assert post.company_name == "some updated company_name"
@@ -68,20 +69,25 @@ defmodule Litelist.PostsTest do
     end
 
     test "update_post/2 with invalid data returns error changeset" do
-      post = post_fixture()
+      post = Factory.insert(:for_sale, @valid_attrs)
       assert {:error, %Ecto.Changeset{}} = Posts.update_post(post, @invalid_attrs)
       assert post == Posts.get_post!(post.id)
     end
 
     test "delete_post/1 deletes the post" do
-      post = post_fixture()
+      post = Factory.insert(:for_sale, @valid_attrs)
       assert {:ok, %Post{}} = Posts.delete_post(post)
       assert_raise Ecto.NoResultsError, fn -> Posts.get_post!(post.id) end
     end
 
     test "change_post/1 returns a post changeset" do
-      post = post_fixture()
+      post = Factory.insert(:for_sale, @valid_attrs)
       assert %Ecto.Changeset{} = Posts.change_post(post)
+    end
+
+    test "posts cannot have identical urls" do
+      Factory.insert(:for_sale, @valid_attrs)
+      assert_raise Ecto.ConstraintError, fn -> Factory.insert(:for_sale, @valid_attrs) end
     end
   end
 end
