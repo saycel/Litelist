30d02c5987f9b50b9c67dbc18d2f626abd54f883
Andy Gimma <andy@hylo.com>
2018-03-25 12:08:39 -0500

    Add seeds, update factories

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,20 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias Litelist.Factory
+
+neighbor = Factory.insert(:neighbor, %{username: "neighbor"})
+admin = Factory.insert(:neighbor, %{username: "admin", admin: true})
+
+Factory.insert_list(2, :job, %{neighbor_id: admin.id})
+Factory.insert_list(2, :event, %{neighbor_id: admin.id})
+Factory.insert_list(2, :business, %{neighbor_id: admin.id})
+Factory.insert_list(2, :emergency_information, %{neighbor_id: admin.id})
+Factory.insert_list(2, :for_sale, %{neighbor_id: admin.id})
+
+Factory.insert_list(2, :job, %{neighbor_id: neighbor.id})
+Factory.insert_list(2, :event, %{neighbor_id: neighbor.id})
+Factory.insert_list(2, :business, %{neighbor_id: neighbor.id})
+Factory.insert_list(2, :emergency_information, %{neighbor_id: neighbor.id})
+Factory.insert_list(2, :for_sale, %{neighbor_id: neighbor.id})
