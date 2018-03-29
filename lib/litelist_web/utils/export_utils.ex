684becae83d63e795b1f339e92e5260ba742fa9f
Andy Gimma <andy@hylo.com>
2018-03-29 02:21:44 -0500

    Add type check in function param

--- lib/litelist_web/utils/export_utils.ex
+++ lib/litelist_web/utils/export_utils.ex
@@ -3,8 +3,14 @@ defmodule LitelistWeb.Utils.ExportUtils do
     Utility functions for exporting data
     """
     alias Litelist.Repo
+    alias Litelist.Auth.Neighbor
 
-    def build_posts_csv(neighbor) do
+    @doc """
+    build_posts_csv(neighbor)
+
+    Runs a SQL query to return a csv of a neighbors posts
+    """
+    def build_posts_csv(%Neighbor{} = neighbor) do
         columns = "id,type,url,title,description,company_name,contact_info,start_date,end_date,start_time,end_time,location,organization_name,position_name,price,salary\n"
 
         stream = Ecto.Adapters.SQL.stream(Repo, "COPY (SELECT #{columns} FROM posts WHERE neighbor_id=#{neighbor.id}) to STDOUT WITH CSV DELIMITER ',' ESCAPE '\"'")
