f1d226315797d996b4d6295771f50c4b3ae0b69c
Andy Gimma <andy@hylo.com>
2018-03-28 23:39:49 -0500

    Remove seed generation from ecto.setup, add ecto.seed. Having seeds as part of setup was messing up the test db

--- mix.exs
+++ mix.exs
@@ -62,8 +62,9 @@ defmodule Litelist.Mixfile do
   # See the documentation for `Mix` for more info on aliases.
   defp aliases do
     [
-      "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
+      "ecto.setup": ["ecto.create", "ecto.migrate"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
+      "ecto.seed": ["run priv/repo/seeds.exs"],
       "test": ["ecto.create --quiet", "ecto.migrate", "test"]
     ]
   end
