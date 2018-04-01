44b20de799f6352a7202135ab1b74eba7eb50bea
Andy Gimma <andy@hylo.com>
2018-04-01 02:32:01 -0500

    Add @doc

--- lib/litelist_web/views/utils_view.ex
+++ lib/litelist_web/views/utils_view.ex
@@ -1,4 +1,15 @@
 defmodule LitelistWeb.UtilsView do
+    @moduledoc """
+    Utility functions for views
+    """
+
+    @doc """
+    Given a resource, this function creates a path that can be used for show, edit or delete.
+    ## Examples
+
+    iex> path_builder(for_sale)
+    "/sales/#{for_sale.id}"
+    """
     def path_builder(resource) do
         case resource.type do
           "for_sale" -> "/sales/#{resource.id}"
