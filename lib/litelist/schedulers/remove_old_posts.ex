230d1901a19d321cc7d57d297691bd271b70a70a
Andy Gimma <andy@hylo.com>
2018-04-01 19:02:49 -0500

    cleanup

--- lib/litelist/schedulers/remove_old_posts.ex
+++ lib/litelist/schedulers/remove_old_posts.ex
@@ -5,12 +5,6 @@ defmodule Litelist.Schedulers.RemoveOldPosts do
     alias Litelist.Posts
 
     def run() do
-        IO.inspect "Running From RemoveOldPosts..."
-        Posts.delete_expired_posts()
-        # delete_posts_past_their_end_date()
-        # delete_posts_past_their_end_time()
-        # delete_old_posts_past_cutoff()
-
-        
+        Posts.delete_expired_posts()        
     end
 end
