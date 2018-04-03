3065a2bfed0ddf474b54cd54ce42d568f4dbe583
Andy Gimma <andy@hylo.com>
2018-04-03 00:55:19 -0500

    fix typo

--- priv/repo/migrations/20180403001934_create_flags.exs
+++ priv/repo/migrations/20180403001934_create_flags.exs
@@ -8,7 +8,7 @@ defmodule Litelist.Repo.Migrations.CreateFlags do
       add :status, :string
       add :admin_response, :text
       add :post_id, references(:posts, on_delete: :nothing)
-      add :neighbor_id, references(:neighbor, on_delete: :nothing)
+      add :neighbor_id, references(:neighbors, on_delete: :nothing)
 
       timestamps()
     end
