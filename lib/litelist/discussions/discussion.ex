aa7763faf766744bd2804d9e8cbcb33a8a40db0a
Andy Gimma <andy@hylo.com>
2018-04-05 21:22:22 -0500

    Add validations

--- lib/litelist/discussions/discussion.ex
+++ lib/litelist/discussions/discussion.ex
@@ -18,10 +18,10 @@ defmodule Litelist.Discussions.Discussion do
   @doc false
   def changeset(%Discussion{} = discussion, attrs) do
     discussion
-    |> cast(attrs, [:title, :description, :neighbor_id])
+    |> cast(attrs, [:title, :description, :neighbor_id, :slug, :url])
     |> unique_constraint(:title, message: "That title already exists. Try another one.")
     |> unique_constraint(:url, message: "That url already exists. Try another one.")
     |> foreign_key_constraint(:discussions, name: :discussions_neighbor_id_fkey)
-    |> validate_required([:title, :description])
+    |> validate_required([:title, :description, :url, :slug])
   end
 end
