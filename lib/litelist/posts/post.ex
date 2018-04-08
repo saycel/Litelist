1a6063e77d36e87b081283d59b74d32f6c0ae86c
Andy Gimma <andy@hylo.com>
2018-04-08 13:06:19 -0500

    Save image url

--- lib/litelist/posts/post.ex
+++ lib/litelist/posts/post.ex
@@ -5,6 +5,7 @@ defmodule Litelist.Posts.Post do
   use Ecto.Schema
   import Ecto.Changeset
   alias Litelist.Posts.Post
+  alias Litelist.Images.Image
 
 
   schema "posts" do
@@ -34,9 +35,14 @@ defmodule Litelist.Posts.Post do
 
   @doc false
   def changeset(%Post{} = post, attrs) do
+    IO.inspect "*****"
+    IO.inspect post
+    IO.inspect attrs
+    IO.inspect "*****"
+
     post
     |> cast(attrs, [:type, :title, :description, :price, :slug, :url, :location, :contact_info, :salary, :position_name, :company_name, :neighbor_id, :start_date, :end_date, :start_time, :end_time, :organization_name])
-    # |> cast_assoc(:images, required: false, with: &Image.changeset/2)
+    |> cast_assoc(:images, required: false, with: &Image.changeset/2)
     |> unique_constraint(:url, message: "That URL already exists. Try another one.")
     |> foreign_key_constraint(:posts, name: :posts_neighbor_id_fkey)
     |> validate_required([:type, :title, :description, :slug, :url, :neighbor_id])
