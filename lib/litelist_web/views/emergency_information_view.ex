ef2145870643e92bfb3a8657b658b16c0cbc29d2
Andy Gimma <andy@hylo.com>
2018-09-04 20:15:14 -0400

    Remove url requirment from posts, also remove from forms

--- lib/litelist_web/views/emergency_information_view.ex
+++ lib/litelist_web/views/emergency_information_view.ex
@@ -42,7 +42,7 @@ defmodule LitelistWeb.EmergencyInformationView do
   		%{select: false, title: "DESCRIPTION", type: "text", po_body: "Describe the item you are trying to sell. Be detailed.", id: "post_description"},
   		%{select: false, title: "CONTACT_INFO", type: "text", po_body: "How can someone contact your business", id: "post_contact"},
   		%{select: false, title: "ORGANIZATION_NAME", type: "text", po_body: "What is the name of this business or company", id: "post_company_name"},
-  		%{select: false, title: "URL", type: "text", po_body: "What URL do you want this post to be found at.  For instance: Ayashas-FORD-BRONCO.4sale.othernet or great-new-car.4sale.othernet", id: "post_url"}
+  		# %{select: false, title: "URL", type: "text", po_body: "What URL do you want this post to be found at.  For instance: Ayashas-FORD-BRONCO.4sale.othernet or great-new-car.4sale.othernet", id: "post_url"}
   	]
   end
 end
