0a9aff28f5d3ee9dcc771c52d170a83074782484
Andy Gimma <andy@hylo.com>
2018-09-12 19:20:48 -0400

    Add neighbor to comment

--- lib/litelist/comments/comment.ex
+++ lib/litelist/comments/comment.ex
@@ -13,12 +13,14 @@ defmodule Litelist.Comments.Comment do
 
     belongs_to :post, Litelist.Posts.Post
     belongs_to :discussion, Litelist.Discussions.Discussion
+    belongs_to :neighbor, Litelist.Auth.Neighbor
+
   end
 
   @doc false
   def changeset(%Comment{} = comment, attrs) do
     comment
-    |> cast(attrs, [:text, :post_id, :discussion_id])
-    |> validate_required([:text])
+    |> cast(attrs, [:text, :post_id, :discussion_id, :neighbor_id])
+    |> validate_required([:text, :neighbor_id])
   end
 end
