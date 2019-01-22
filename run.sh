cad299db5efa13866fa13bcf454a490a7ef1a424
Andy Gimma <andy@hylo.com>
2019-01-22 18:05:13 -0500

    Add Amnesia db

--- run.sh
+++ run.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 # add & to make these next 2 run in the background
-
-# mix amnesia.create --database Litelist.Settings.SettingsDatabase --disk
-# MIX_ENV=test mix amnesia.create --database Litelist.Settings.SettingsDatabase --disk
+echo "Adding amnesia database (these will soon be removed)..."
+mix amnesia.create --database Litelist.Settings.SettingsDatabase --disk
+MIX_ENV=test mix amnesia.create --database Litelist.Settings.SettingsDatabase --disk
 
 set -e
 
+echo "Running npm install..."
 cd assets && npm install
 
+echo "Running mix setup commands..."
 cd ..
 mix deps.get
 mix ecto.create
 mix ecto.migrate
 
-# Start the phoenix web server
+echo "Starting server..."
 mix phx.server
